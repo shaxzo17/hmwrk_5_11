b24222a98b866f4eaf47aa45011423be3544b599
shaxzo17 <shaxzodaavalboyeva14@gmail.com>
2025-06-21 14:16:53 +0300

    Uy ishi

---
+++ main.py
@@ -0,0 +1,27 @@
+# Short Long Short
+# def solution(a, b):
+#     if len(a) < len(b):
+#         return a + b + a
+#     else:
+#         return b + a + b
+
+
+# Correct the mistakes of the character recognition software
+# def correct(s):
+#     return s.replace('5', 'S').replace('0', 'O').replace('1', 'I')
+
+
+# Fake Binary
+# def fake_bin(x):
+#     return ''.join('0' if int(d) < 5 else '1' for d in x)
+
+
+# Abbreviate a Two Word Name
+# def abbrev_name(name):
+    # first, last = name.split()
+    # return f"{first[0].upper()}.{last[0].upper()}"
+
+
+# Convert a string to an array
+# def string_to_array(s):
+    # return s.split(" ")
